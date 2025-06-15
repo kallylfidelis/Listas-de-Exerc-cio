e44dda786dbb5fb2219952196f0384c1dd1069e4
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:28:34 -0300

    ex 6

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var inputValor1 = document.querySelector('#inputValor1');
-var inputValor2 = document.querySelector('#inputValor2');
-var btCalcular = document.querySelector('#btCalcular');
-var h3Resultado = document.querySelector('#h3Resultado');
+var inputValor1 = document.querySelector("#inputValor1")
+var inputValor2 = document.querySelector("#inputValor2")
+var inputValor3 = document.querySelector("#inputValor3")
+var inputValor1 = document.querySelector("#inputValor3")
+var inputValor1 = document.querySelector("#btCalcular")
+var inputValor1 = document.querySelector("#h3Resultado")
 
-function calcularMaiorValor() {
-    var valor1 = Number(inputValor1.value);
-    var valor2 = Number(inputValor2.value);
-    if (isNaN(valor1) || isNaN(valor2)) {
-        h3Resultado.textContent = "Por favor, insira valores válidos.";
-        return;
-    }
-    var maiorValor = valor1 > valor2 ? valor1 : valor2;
-    h3Resultado.textContent = "O maior valor é: " + maiorValor;
+
+function calcularMaiorValor (){
+    var valor1 = Number (inputValor1.value)
+    var valor2 = Number (inputValor2.value)
+    var valor3 = Number (inputValor3.value)
+    var valor4 = Number (inputValor4.value)
+
+    var maiorValor = valor1 > valor2 > valor3 > valor4 ? valor1 : valor3 
+    h3Resultado.textContent = "seu maior valor é: " + maiorValor
 }
+
 btCalcular.onclick = function() {
-    calcularMaiorValor();
+    calcularMaiorValor()
 }
